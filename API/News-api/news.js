dc97658d00e47d05a80833ebb2021a743f558957
ShahbajKhan <shktoky@gmail.com>
2023-04-04 15:47:16 +0600

    practiced news-portal api porject

--- API/News-api/news.js
+++ API/News-api/news.js
@@ -1,4 +1,5 @@
 const loadCategory = () => {
+    
   fetch("https://openapi.programming-hero.com/api/news/categories")
     .then((res) => res.json())
     .then((data) => showCategory(data.data.news_category));
